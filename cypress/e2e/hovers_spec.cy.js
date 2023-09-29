f7916eae9b4f868e3f1c9f94d14c8ef2dd860d36
Bruno Araujo <lbruno.araujo@hotmail.com>
2023-09-29 11:54:16 -0300

    Finished Hovers

--- cypress/e2e/hovers_spec.cy.js
+++ cypress/e2e/hovers_spec.cy.js
@@ -11,40 +11,65 @@ describe(`Testar componentes do link Hovers na url ${Cypress.config().baseUrl}`,
     })
 
 
+ const list_links = [
+                      {"url": "/users/1", "index": 0, "msg": "user1"},
+                      {"url": "/users/2", "index": 1, "msg": "user2"},
+                      {"url": "/users/3", "index": 2, "msg": "user3"}
+                    ].forEach( link => {
 
-    context.skip('Dado o click no Hovers devo ser redirecionado para página', ()=>{
+                        context('Dado o click no Hovers devo ser redirecionado para página', ()=>{
 
                           
-        it('Então clico no hover user1 e clico no link View Profile e valido o redirecionamento a url', ()=>{
+                            it(`Então clico no hover ${link.url} e clico no link View Profile e valido o redirecionamento a url`, ()=>{
+                    
+                                cy.assertPage('Hovers', 'hovers','.example > h3', 'Hovers') 
+                            
+                    
+                                cy.url().then( url => {
+                    
+                                    expect(url).equal(Cypress.config().baseUrl.concat('hovers'))
+                    
+                                    cy.get('.example > h3').then( (el)=>{
+                                        expect(el[0].innerText).equal('Hovers')
+                                    })
+                                })
+                    
+                                cy.get('.figcaption').then( el => {
+                                    
+                                    cy.wrap(el[link.index]).invoke('show')
+                                    expect(el[link.index].innerText).equal(`\n      name: ${link.msg}\n      View profile\n    `)
+                        
+                                })
+                    
+                                
+                                cy.get('.figcaption').find('a').then(el => {
+                                 
+                                    cy.wrap(el[link.index]).as('link_view_profile')
+                    
+                                    cy.get('@link_view_profile')
+                                    .invoke('attr', 'href')
+                                    .should('equal', link.url)
+                                    
+                                    cy.get('@link_view_profile').click()
+                                            
+                                 })
 
-            cy.contains('Hovers').should('be.visible').click()  
-
-            cy.url().then( url => {
-
-                expect(url).equal(Cypress.config().baseUrl.concat('hovers'))
-
-                cy.get('.example > h3').then( (el)=>{
-                    expect(el[0].innerText).equal('Hovers')
-                })
-            })
-
-            // cy.get('.figure').trigger('mouseover')  
-
-            // cy.get('.figcaption > h5').then( el => {
-            //     expect(el[0].innerText).equal('name: '.concat('user1'))
-
-            // })
-    
-            
-                // cy.get('.figcaption > a').then( el => {
+                                 
+                                 cy.url().then( el => {
+                    
+                                    expect(Cypress.config().baseUrl.concat(link.url.slice(1))).equal(el)
                 
-                //     cy.wrap(el[0]).invoke('show').click({force: true})
-                // })
+                                })
+                            })
+                        })
 
-            
-            
+                    })   
 
 
-        })
-        })
 })
+
+
+
+
+
+  
